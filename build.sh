112c0747aa882b35a294dcd47d2b4001b1a39c38
Lei Dando <ashleiluiezdando26@gmail.com>
2026-04-02 18:34:46 +0800

    Fix Render deployment: downgrade python-bidi and set python 3.11

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 # Render Build Script for CSWDO Management System
 set -o errexit
 
+# Use the virtualenv provided by Render
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Ensure we're using binary versions of critical packages if possible
+pip install --only-binary :all: psycopg2-binary || true
 
 python manage.py collectstatic --no-input
 python manage.py migrate
